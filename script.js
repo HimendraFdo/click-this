3cea9761add5ff24c3b8697095d77f484c05b490
Himendra <fernandohimendra@gmail.com>
2026-02-11 16:41:26 +1300

    made changes to make the site work again

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const mainScreen = document.getElementById("main-screen");
@@ -14,21 +13,4 @@ noBtn.addEventListener("mouseover", () => {
     const y = Math.random() * (window.innerHeight - 100);
     noBtn.style.left = `${x}px`;
     noBtn.style.top = `${y}px`;
-=======
-const yesBtn = document.getElementById("yesBtn");
-const noBtn = document.getElementById("noBtn");
-const mainScreen = document.getElementById("main-screen");
-const yesScreen = document.getElementById("yes-screen");
-
-yesBtn.addEventListener("click", () => {
-    mainScreen.classList.add("hidden");
-    yesScreen.classList.remove("hidden");
-});
-
-noBtn.addEventListener("mouseover", () => {
-    const x = Math.random() * (window.innerWidth - 100);
-    const y = Math.random() * (window.innerHeight - 100);
-    noBtn.style.left = `${x}px`;
-    noBtn.style.top = `${y}px`;
->>>>>>> f7d5403e95a0b80653619d37b22f5e90ab388a08
 });
